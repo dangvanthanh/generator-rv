39746602a7917a6545dead0ab0db39bb62c9de33
Dang Van Thanh <dangvanthanh@dangthanh.org>
2018-04-01 16:55:18 +0700

    Add prettier

--- template/rollup.config.dev.js
+++ template/rollup.config.dev.js
@@ -1,8 +1,8 @@
-import alias from 'rollup-plugin-alias'
-import vue from 'rollup-plugin-vue'
-import buble from 'rollup-plugin-buble'
-import nodeResolve from 'rollup-plugin-node-resolve'
-import commonjs from 'rollup-plugin-commonjs'
+import alias from 'rollup-plugin-alias';
+import vue from 'rollup-plugin-vue';
+import buble from 'rollup-plugin-buble';
+import nodeResolve from 'rollup-plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs';
 
 export default {
   input: 'src/index.js',
@@ -23,4 +23,4 @@ export default {
     nodeResolve({ browser: true, jsnext: true, main: true }),
     commonjs()
   ]
-}
+};
